ced17e6779634266db4117be9c2c74f80a02f2e2
Vlad <pleo12@gmail.com>
2016-07-21 14:27:52 +0300

    lockv10 update

--- lock/package.js
+++ lock/package.js
@@ -2,25 +2,22 @@
 
 Package.describe({
     name         : 'auth0:lock',
-    version      : '0.5.0',
+    version      : '0.6.0',
     summary      : 'Auth0 Lock for Meteor',
     git          : 'https://github.com/auth0/meteor-auth0.git',
     documentation: 'README.md'
 });
 
 Npm.depends({
-    "auth0-lock": "9.0.0",
-    "brfs"      : "1.4.3",
-    "ejsify"    : "0.1.0",
-    "packageify": "0.2.0"
+    "auth0-lock": "10.0.0-rc.2",
+    "events"    : "1.1.1",
+    "util"      : "0.10.3"
 });
 
 Package.onUse(function (api) {
     api.versionsFrom('1.2.1');
-    api.use(['ecmascript', 'accounts-base', 'accounts-oauth']);
-    api.use(['underscore'], 'server');
-    api.use(['cosmos:browserify@0.9.2'], 'client');
-    api.addFiles(['auth0-lock.browserify.js', 'auth0-lock.js'], 'client');
+    api.use(['ecmascript', 'accounts-base', 'accounts-oauth', 'underscore']);
+    api.addFiles(['auth0-lock.js'], 'client');
     api.addFiles(['auth0-lock.server.js'], 'server');
     api.addFiles(['auth0-lock.common.js']);
 
